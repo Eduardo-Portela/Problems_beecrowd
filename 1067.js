526a188e5c816261c84a3dd48a656ef643e641d2
Eduardo Portela <eduardo.portela.jrr@gmail.com>
2023-10-30 20:40:05 -0300

    1067 complete

---
+++ 1067.js
@@ -0,0 +1,13 @@
+const path = require('path')
+const currentFile = path.dirname(process.argv[1])
+
+var input = require('fs').readFileSync(currentFile + '\\dev\\stdin', 'utf8')
+var lines = input.split('\r\n')
+
+let value = Number(lines.shift());
+
+for(let i = 0; i <= value; i++){
+    if(i % 2 != 0){
+        console.log(i)
+    }
+}
