1ec1e12905de536d4a9f35c3b6c1b7f46b6bb44a
Eduardo Portela <eduardo.portela.jrr@gmail.com>
2023-11-01 09:31:36 -0300

    1113 complete

---
+++ 1113.js
@@ -0,0 +1,18 @@
+const path = require('path')
+const currentFile = path.dirname(process.argv[1])
+
+var input = require('fs').readFileSync(currentFile + '\\dev\\stdin', 'utf8')
+var lines = input.split('\r\n')
+let line1 = 0
+let line2 = 0
+
+for(let i = 0; i < lines.length; i++){
+    let line = lines[i].split(" ")
+    line1 = Number(line.shift())
+    line2 = Number(line.shift())
+    if(line1 > line2){
+        console.log("Decrescente")
+    }else if(line1 < line2){
+        console.log("Crescente")
+    }
+}
