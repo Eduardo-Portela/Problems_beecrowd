5e9e276595b161b8e7ca079a8a70b38e0c8a9747
Eduardo Portela <eduardo.portela.jrr@gmail.com>
2023-11-22 17:42:57 -0300

    1181 complete

---
+++ 1181.js
@@ -0,0 +1,42 @@
+const path = require('path')
+const currentFile = path.dirname(process.argv[1])
+
+var input = require('fs').readFileSync(currentFile + '\\dev\\stdin', 'utf8')
+var lines = input.split('\r\n')
+
+let line = Number(lines.shift())
+let type = lines.shift()
+let currentValue = 0
+let matriz = []
+let lins = 12
+let cols = 12
+
+
+do {
+    
+    for(let i = 0; i < lins; i++){
+        const newLine = []
+        
+        for(let j = 0; j < cols; j++){
+            currentValue = parseFloat(lines.shift())
+            newLine.push(currentValue)
+        }
+        matriz.push(newLine)
+    }
+
+}while(lines.length != 0)
+
+let soma = 0
+let media = 0
+for(let i = 0; i < matriz[line].length; i++){
+    if(type === "S"){
+        soma += matriz[line][i]
+    }else{
+        soma += matriz[line][i]
+    }
+}
+media = soma / 12
+
+type === "S" ? console.log(soma.toFixed(1)) : console.log(media.toFixed(1))
+
+// console.log(matriz)
