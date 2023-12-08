f8520779b76aceb64c20ba3813095093129db70a
Eduardo Portela <eduardo.portela.jrr@gmail.com>
2023-12-08 12:57:34 -0300

    2702 complete

---
+++ 2702.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const currentFile = path.dirname(process.argv[1])
+
+var input = require('fs').readFileSync(currentFile + '\\dev\\stdin', 'utf8')
+var lines = input.split('\r\n')
+const cases = lines.length
+let chicken
+let pasta
+let meat
+
+let avaliable = lines.shift().split(" ").map(Number)
+let requests = lines.shift().split(" ").map(Number)
+chicken = avaliable[0] - requests[0] 
+pasta = avaliable[1] - requests[1] 
+meat = avaliable[2] - requests[2]
+
+const arrResult = [chicken, pasta, meat]
+let requestsDenied = 0
+
+arrResult.forEach((element) => {
+    if(element < 0){
+        requestsDenied += element
+    }
+})
+
+console.log(Math.abs(requestsDenied))
+
