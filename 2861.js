631a4e2fcd3a5d2e33a476e0bc00530efd953bb0
Eduardo Portela <eduardo.portela.jrr@gmail.com>
2023-12-12 13:17:37 -0300

    2861 complete

---
+++ 2861.js
@@ -0,0 +1,11 @@
+const path = require('path')
+const currentFile = path.dirname(process.argv[1])
+
+var input = require('fs').readFileSync(currentFile + '\\dev\\stdin', 'utf8')
+var lines = input.split('\r\n')
+
+const cases = Number(lines.shift())
+
+for(let i = 0; i < cases; i++){
+    console.log("gzuz")
+}
