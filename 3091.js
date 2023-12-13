a0b5c381079251ce14752846bb664197fe6c5d5f
Eduardo Portela <eduardo.portela.jrr@gmail.com>
2023-12-13 15:21:53 -0300

    3091 complete

---
+++ 3091.js
@@ -0,0 +1,9 @@
+const path = require('path')
+const currentFile = path.dirname(process.argv[1])
+
+var input = require('fs').readFileSync(currentFile + '\\dev\\stdin', 'utf8')
+var lines = input.split('\r\n')
+
+const [a,b] = lines.map(Number)
+
+console.log(a % b)
