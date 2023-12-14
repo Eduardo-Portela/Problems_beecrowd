b53ea21e525af676c0f18864dc48b19e05366a11
Eduardo Portela <eduardo.portela.jrr@gmail.com>
2023-12-14 13:01:15 -0300

    1984.js

---
+++ 1984.js
@@ -0,0 +1,8 @@
+const path = require('path')
+const currentFile = path.dirname(process.argv[1])
+
+var input = require('fs').readFileSync(currentFile + '\\dev\\stdin', 'utf8')
+var lines = input.split('\r\n')
+
+const N = lines.shift().split("");
+console.log(N.reverse().join(""))
